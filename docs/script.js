88ea802f83c914bd0a7f3a9a77c0df54e215a3a7
Noah Casarotto-Dinning <noahcasarotto-dinning@Noahs-MacBook-Pro.local>
2023-10-30 20:11:43 -0400

    Added Assignment games button

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Welcome to My Website!');
+    
+    // Event listener for the Assignment Games button
+    document.getElementById('assignment-games-btn').addEventListener('click', function() {
+        window.location.href = 'url-to-assignment-games.html'; // Replace with the actual URL
+    });
 });
+
